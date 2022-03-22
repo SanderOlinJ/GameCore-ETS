37fd18b583c3de187f6bca192c95b6f093a047bf
DeDcon0 <bekadb@ntnu.no>
2022-03-22 16:05:05 +0100

    added Main page controller

--- src/main/java/edu/ntnu/idatt1002/sysdev_k1_05_ets/controllers/MainPageController.java
+++ src/main/java/edu/ntnu/idatt1002/sysdev_k1_05_ets/controllers/MainPageController.java
@@ -1,4 +1,26 @@
 package edu.ntnu.idatt1002.sysdev_k1_05_ets.controllers;
 
+import edu.ntnu.idatt1002.sysdev_k1_05_ets.MainApplication;
+import edu.ntnu.idatt1002.sysdev_k1_05_ets.tournament.Bracket;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class MainPageController {
+    Bracket newBracket;
+
+    @FXML
+    public void OnCreateNewTournamentClick(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(MainApplication.class.getResource("eight_team_bracket.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
