e2525dbfe4ca5f8f974ffbab092ea446476ce92b
DeDcon0 <93771670+BexLeetBox@users.noreply.github.com>
2022-03-22 12:15:21 +0100

    added page redirection to eight team bracket from adding teams

--- src/main/java/edu/ntnu/idatt1002/sysdev_k1_05_ets/controllers/AddTeamController.java
+++ src/main/java/edu/ntnu/idatt1002/sysdev_k1_05_ets/controllers/AddTeamController.java
@@ -1,21 +1,28 @@
 package edu.ntnu.idatt1002.sysdev_k1_05_ets.controllers;
+import edu.ntnu.idatt1002.sysdev_k1_05_ets.MainApplication;
 import edu.ntnu.idatt1002.sysdev_k1_05_ets.tournament.Team;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class AddTeamController {
     ArrayList<Team> allTeams = new ArrayList<>();
-    private Scene mainScene;
+
+    private Scene scene;
+    private Parent root;
+    private Stage stage;
 
     @FXML
     TextField teamNameField;
@@ -27,9 +34,13 @@ public class AddTeamController {
     Label warningLabel;
 
 
-
-    public void setMainScene(Scene scene) {
-        mainScene = scene;
+    @FXML
+    public void setMainScene(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(MainApplication.class.getResource("eight_team_bracket.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     public void addTeam(ActionEvent actionEvent){
@@ -63,6 +74,5 @@ public class AddTeamController {
 
     public void openMainScene(ActionEvent actionEvent) {
         Stage primaryStage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
-        primaryStage.setScene(mainScene);
     }
 }
