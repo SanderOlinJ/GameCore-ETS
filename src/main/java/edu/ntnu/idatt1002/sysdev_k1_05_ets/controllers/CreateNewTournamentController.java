45706f51091967f412d32e14ff477008c95cd092
DeDcon0 <bekadb@ntnu.no>
2022-03-23 14:52:48 +0100

    load tournament loads a tournament with a list of teams

--- src/main/java/edu/ntnu/idatt1002/sysdev_k1_05_ets/controllers/CreateNewTournamentController.java
+++ src/main/java/edu/ntnu/idatt1002/sysdev_k1_05_ets/controllers/CreateNewTournamentController.java
@@ -1,15 +1,25 @@
 package edu.ntnu.idatt1002.sysdev_k1_05_ets.controllers;
 
+import edu.ntnu.idatt1002.sysdev_k1_05_ets.MainApplication;
 import edu.ntnu.idatt1002.sysdev_k1_05_ets.team_file_managers.TeamReader;
 import edu.ntnu.idatt1002.sysdev_k1_05_ets.tournament.Team;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class CreateNewTournamentController {
+
+    private Scene scene;
+    private Parent root;
+    private Stage stage;
 
     @FXML
     TextField tournamentNameField;
@@ -21,6 +31,15 @@ public class CreateNewTournamentController {
         TeamReader TR = new TeamReader();
         String tournamentName = tournamentNameField.getText();
         teamsNameFieldWithPlayerNames.setText(TR.convertTournamentFileToText(tournamentName));
+    }
+
+    @FXML
+    public void addTeamScene(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(MainApplication.class.getResource("scenes/add-team-scene.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
 
