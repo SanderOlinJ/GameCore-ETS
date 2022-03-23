abddab7ddfefa9a719eefe745ab1a9c0aee99aaf
Joakim <joakifa@stud.ntnu.no>
2022-03-23 12:17:44 +0100

    Worked on controllers and new tournament scene

--- src/main/java/edu/ntnu/idatt1002/sysdev_k1_05_ets/controllers/AddTeamController.java
+++ src/main/java/edu/ntnu/idatt1002/sysdev_k1_05_ets/controllers/AddTeamController.java
@@ -1,5 +1,6 @@
 package edu.ntnu.idatt1002.sysdev_k1_05_ets.controllers;
 import edu.ntnu.idatt1002.sysdev_k1_05_ets.MainApplication;
+import edu.ntnu.idatt1002.sysdev_k1_05_ets.TeamWriter;
 import edu.ntnu.idatt1002.sysdev_k1_05_ets.tournament.Team;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -22,6 +23,7 @@ public class AddTeamController {
     private Scene scene;
     private Parent root;
     private Stage stage;
+    private ArrayList<Team> addedTeams;
 
     @FXML
     TextField teamNameField;
