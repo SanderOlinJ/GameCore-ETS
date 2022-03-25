b418d5f66f74b58fb2857098cf560dda8842607b
DeDcon0 <93771670+BexLeetBox@users.noreply.github.com>
2022-03-25 11:16:15 +0100

    reworked create tournament page, button has width limit and added a combobox

--- src/main/java/edu/ntnu/idatt1002/sysdev_k1_05_ets/controllers/CreateNewTournamentController.java
+++ src/main/java/edu/ntnu/idatt1002/sysdev_k1_05_ets/controllers/CreateNewTournamentController.java
@@ -6,16 +6,20 @@ import edu.ntnu.idatt1002.sysdev_k1_05_ets.tournament.Team;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class CreateNewTournamentController {
+public class CreateNewTournamentController implements Initializable {
 
     private Scene scene;
     private Parent root;
@@ -25,7 +29,13 @@ public class CreateNewTournamentController {
     TextField tournamentNameField;
     @FXML
     TextArea teamsNameFieldWithPlayerNames;
+    @FXML
+    ComboBox bracketFormat;
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    bracketFormat.getItems().addAll("4","8","16");
+    }
     @FXML
     public void onLoadTournamentClick(ActionEvent event) throws IOException {
         TeamReader TR = new TeamReader();
@@ -50,6 +60,7 @@ public class CreateNewTournamentController {
         stage.setScene(scene);
         stage.show();
     }
+
 
 
 }
