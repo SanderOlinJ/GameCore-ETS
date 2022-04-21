f53f930fc81781986b89e773a754a70779b933dc
Håkon Bergrem <haakohbe@ntnu.no>
2022-04-21 18:14:07 +0200

    Added tests to NewTournament

---
+++ src/test/java/edu/ntnu/idatt1002/sysdev_k1_05_ets/Tournament/NewTournamentTest.java
@@ -0,0 +1,103 @@
+package edu.ntnu.idatt1002.sysdev_k1_05_ets.Tournament;
+
+import edu.ntnu.idatt1002.sysdev_k1_05_ets.tournament.Match;
+import edu.ntnu.idatt1002.sysdev_k1_05_ets.tournament.NewTournament;
+import edu.ntnu.idatt1002.sysdev_k1_05_ets.tournament.Team;
+import edu.ntnu.idatt1002.sysdev_k1_05_ets.tournament.Tournament;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NewTournamentTest {
+
+    @Test
+    void teamsIsNullInConstructor(){
+        ArrayList<Team> teams = new ArrayList<Team>();
+        try{
+            NewTournament newTournament = new NewTournament("Upcoming", "MadTournament",
+                    "Admin",  LocalDate.parse("2022-04-25"),  LocalTime.parse("16:15:12"),
+                    "A bloody insane tournament", "Fortnite", "PS5", "Brackets",
+                    "1", "4", "10000", "NOK", "0",
+                    "NOK", teams);
+        }catch(IllegalArgumentException e){
+            assertEquals("Teams cannot be empty!", e.getMessage());
+        }
+
+    }
+
+    @Test
+    void findNextMatchToBePlayedTest(){
+        Team team1 = new Team("team1");
+        Team team2 = new Team("Team2");
+        Team team3 = new Team("Team3");
+        Team team4 = new Team("Team4");
+
+        ArrayList<Team> teams = new ArrayList<Team>();
+        teams.add(team1);
+        teams.add(team2);
+        teams.add(team3);
+        teams.add(team4);
+
+        ArrayList<Match> matches = new ArrayList<Match>();
+        Match match1 = new Match(team1, team2);
+        Match match2 = new Match(team3, team4);
+        matches.add(match1);
+        matches.add(match2);
+        NewTournament newTournament = new NewTournament("Upcoming", "MadTournament",
+                    "Admin",  LocalDate.parse("2022-04-25"),  LocalTime.parse("16:15:12"),
+                    "A bloody insane tournament", "Fortnite", "PS5", "Brackets",
+                    "1", "4", "10000", "NOK", "0",
+                    "NOK", teams);
+        for(Match match : matches){
+            match.setFinished(false);
+            match.setTimeOfMatch(LocalTime.parse("00:00:00"));
+        }
+
+        match1.setDateOfMatch(LocalDate.parse("2022-12-12"));
+        match2.setDateOfMatch(LocalDate.parse("2023-12-12"));
+
+        newTournament.setMatches(matches);
+        assertEquals(match1, newTournament.findNextMatchToBePlayed());
+    }
+
+    @Test
+    void findNumberOfTeamsLeftTest(){
+        Team team1 = new Team("team1");
+        Team team2 = new Team("Team2");
+        Team team3 = new Team("Team3");
+        Team team4 = new Team("Team4");
+
+        ArrayList<Team> teams = new ArrayList<Team>();
+        teams.add(team1);
+        teams.add(team2);
+        teams.add(team3);
+        teams.add(team4);
+
+        ArrayList<Match> matches = new ArrayList<Match>();
+        Match match1 = new Match(team1, team2);
+        Match match2 = new Match(team3, team4);
+        matches.add(match1);
+        matches.add(match2);
+        NewTournament newTournament = new NewTournament("Upcoming", "MadTournament",
+                "Admin",  LocalDate.parse("2022-04-25"),  LocalTime.parse("16:15:12"),
+                "A bloody insane tournament", "Fortnite", "PS5", "Brackets",
+                "1", "4", "10000", "NOK", "0",
+                "NOK", teams);
+        for(Match match : matches){
+            match.setFinished(false);
+            match.setTimeOfMatch(LocalTime.parse("00:00:00"));
+        }
+
+        match1.setDateOfMatch(LocalDate.parse("2022-12-12"));
+        match2.setDateOfMatch(LocalDate.parse("2023-12-12"));
+
+        match1.setFinished(true);
+
+        newTournament.setMatches(matches);
+        assertEquals(3, newTournament.findNumberOfTeamsLeft());
+    }
+}
