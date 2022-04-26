eab4673c197107759921330a7e30ab49edccd2ca
DeDcon0 <93771670+BexLeetBox@users.noreply.github.com>
2022-04-26 19:43:52 +0200

    Text corrections in about page and more javadoc

--- src/main/java/edu/ntnu/idatt1002/sysdev_k1_05_ets/controllers/AboutAndHelpController.java
+++ src/main/java/edu/ntnu/idatt1002/sysdev_k1_05_ets/controllers/AboutAndHelpController.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class AboutAndHelpController {
 
     /**
-     * Redirects to main page if home menu button
+     * Redirects to main page if home menu button is clicked
      * @throws IOException
      */
     @FXML
@@ -19,31 +19,61 @@ public class AboutAndHelpController {
     throws IOException {
         ViewSwitcher.switchTo(View.MAIN);
     }
+
+    /**
+     * Redirects to about page if clicked on about menu button
+     * @throws IOException
+     */
     @FXML
     void onAboutButtonPressed()
     throws IOException {
         ViewSwitcher.switchTo(View.ABOUT);
     }
+
+    /**
+     * Redirects to help page if clicked on help menu button
+     * @throws IOException
+     */
     @FXML
     void onHelpButtonPressed()
     throws IOException {
         ViewSwitcher.switchTo(View.HELP);
     }
+
+    /**
+     * Redirects to on going tournament page if clicked on ongoing  menu button
+     * @throws IOException
+     */
     @FXML
     void onOngoingTournamentsButtonPressed()
     throws IOException {
         ViewSwitcher.switchTo(View.ONGOING_OVERVIEW);
     }
+
+    /**
+     * Redirects to ongoing tournament page if clicked on ongoing  menu button
+     * @throws IOException
+     */
     @FXML
     void onUpcomingTournamentsButtonPressed()
     throws IOException{
         ViewSwitcher.switchTo(View.UPCOMING_OVERVIEW);
     }
+
+    /**
+     * Redirects to previous tournament page if clicked on previous tournaments menu button
+     * @throws IOException
+     */
     @FXML
     void onPreviousTournamentsButtonPressed()
     throws IOException{
         ViewSwitcher.switchTo(View.PREVIOUS_OVERVIEW);
     }
+
+    /**
+     * Redirects to create new tournament page if clicked on create new tournament button
+     * @throws IOException
+     */
     @FXML
     void onCreateNewTournamentButtonClicked()
     throws IOException{
