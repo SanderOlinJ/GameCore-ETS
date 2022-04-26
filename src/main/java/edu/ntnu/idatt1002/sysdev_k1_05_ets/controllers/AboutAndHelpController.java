3e553973cc3d411e0feacb353d3af42c618fa69d
DeDcon0 <93771670+BexLeetBox@users.noreply.github.com>
2022-04-26 18:07:57 +0200

    Text corrections in about page and more javadoc

--- src/main/java/edu/ntnu/idatt1002/sysdev_k1_05_ets/controllers/AboutAndHelpController.java
+++ src/main/java/edu/ntnu/idatt1002/sysdev_k1_05_ets/controllers/AboutAndHelpController.java
@@ -5,8 +5,15 @@ import edu.ntnu.idatt1002.sysdev_k1_05_ets.scenes.ViewSwitcher;
 import javafx.fxml.FXML;
 import java.io.IOException;
 
+/**
+ * A controller class for both about and help page
+ */
 public class AboutAndHelpController {
 
+    /**
+     * Redirects to main page if home menu button
+     * @throws IOException
+     */
     @FXML
     void onHomeButtonPressed()
     throws IOException {
--- src/main/java/edu/ntnu/idatt1002/sysdev_k1_05_ets/controllers/StartScreenController.java
+++ src/main/java/edu/ntnu/idatt1002/sysdev_k1_05_ets/controllers/StartScreenController.java
@@ -7,10 +7,20 @@ import javafx.scene.control.CheckBox;
 
 import java.io.IOException;
 
+/**
+ * A controller class for the first page a user sees when launching ETS
+ */
 public class StartScreenController {
 
     @FXML private CheckBox firstTimeCheckBox;
 
+    /**
+     * Checks if the checkbox which asks the user if it is their first time, is checked
+     * If "first time" check box is checked the page gets redirected to the help page of ETS
+     * If "first time" checkbox is not checked the page gets redirected to the main page where you see upcoming, ongoing
+     * and previous tournaments
+     * @throws IOException
+     */
     @FXML
     void onStartButtonPressed()
     throws IOException {
